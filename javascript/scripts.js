394edeeebecdc239a5441256f9b4d46b1063e3be
Marcos <marcfreixanet@gmail.com>
2022-10-14 23:26:00 -0700

    added some js to keep the buttons active until another button is clicked

--- javascript/scripts.js
+++ javascript/scripts.js
@@ -1,4 +1,7 @@
 var submitButton = document.querySelector(".submit-button");
+var buttons = document.querySelectorAll(".button");
+var activeClassName = 'active';
+
 
 submitButton.addEventListener('click', changeCard);
 
@@ -6,3 +9,23 @@ function changeCard() {
     var ratingCard = document.querySelector(".rating-container").style.display = "none";
     var thanksCard = document.querySelector(".thanks-container").style.display = "flex";
 }
+
+
+function activeState(items, activeName) {
+    for(var i = 0; i < items.length; i++) {
+        if(items[i].classList.contains(activeName)) {
+            items[i].classList.remove(activeName); 
+        }
+    }  
+}
+
+for(var i = 0; i < buttons.length; i++) {
+    buttons[i].addEventListener('click', function(e){
+      activeState(buttons, activeClassName);
+      e.target.classList.add(activeClassName);
+    });
+  }
+
+
+
+
